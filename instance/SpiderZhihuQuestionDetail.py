c68678f42de4c8701b9afc10f1f5dd86480c7aa7
闵崇建 <chongjian.min@yuanpinzhuyi.com>
2016-03-05 18:14:22 +0800

    1.将爬取到的知乎话题和回复经过处理后保存至数据库

--- instance/SpiderZhihuQuestionDetail.py
+++ instance/SpiderZhihuQuestionDetail.py
@@ -1,9 +1,11 @@
 import urllib
 import urllib.request
 import re
-import pymysql
+import pymysql.cursors
 
-spiderUrl = 'https://www.zhihu.com/question/39880275'
+PLATFORAM_NAME = "zhihu";
+
+spiderUrl = 'https://www.zhihu.com/question/30220049'
 response = urllib.request.urlopen(spiderUrl)
 responseData = response.read().decode()
 
@@ -102,107 +104,182 @@ def getQuestionAnswers(responseData):
 
 # 知乎问题ID
 questionId = getQuestionId(responseData)
-
 # 获取问题标题
 questionTitle = getQuestionTitle(responseData)
-
 # 获取问题补充
 questionSupport = getQuestionSupport(responseData)
-print("questionId=", questionId, ",questionTitle=", questionTitle, ",questionSupport=", questionSupport);
 
-# 获取答案
-# getQuestionAnswers(responseData)
+topic = {
+    "questionId": questionId,
+    "questionTitle": questionTitle,
+    "questionSupport": questionSupport
+}
+# print(topic)
 
 # 获取知乎问题信息(问题ID,点赞数量,答案作者,作者签名,问题答案,创建时间,编辑时间)
 answerList = getQuestionAnswers(responseData)
-print("answerList:", answerList)
-
-
+# print("answerList:", answerList)
 
 
 # ------------------------------------- 将搜集到的数据,插入到数据库 ------------------------------------#
+def getConn():
+    conn = pymysql.connect(host='127.0.0.1',
+                           user='root',
+                           password='123456',
+                           db='pandave',
+                           charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor)
+    return conn
 
 
 # ------------------------------------- 插入话题信息 -------------------------------------#
+topicId = None;
+
+
 # 根据platform_pid和platform_name查询话题是否已经存在
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def handleTopic(topic):
+    questionId = topic["questionId"]
+    questionTitle = topic["questionTitle"]
+    questionSupport = topic["questionSupport"]
+
+    conn = getConn()
+    try:
+        with conn.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT id,title,content,author_name,last_follow_time FROM pandave_topic where platform_tid =%s and platform_name=%s"
+            cursor.execute(sql, (questionId, PLATFORAM_NAME))
+            result = cursor.fetchone()
+            # print("result", result)
+
+            # 如果不存在,则新建一条记录
+            # 1. questionId
+            # 2. platformName
+            # 3. questionTitle
+            # 4. questionSupport
+            if (result == None):
+                print(questionId, "不存在,插入数据到pandave.")
+                sql = "INSERT INTO pandave_topic (creator, gmt_create, modifier, gmt_modified, is_deleted, platform_tid, platform_name, title, content, last_follow_time) " \
+                      "VALUES (1, now(), 1, now(), 'N', %s, %s, %s, %s, now());"
+                cursor.execute(sql, (questionId, PLATFORAM_NAME, questionTitle, questionSupport))
+                conn.commit()
+
+                # 查询出topicId
+                sql = "SELECT id,title,content,author_name,last_follow_time FROM pandave_topic where platform_tid =%s and platform_name=%s"
+                cursor.execute(sql, (questionId, PLATFORAM_NAME))
+                result = cursor.fetchone()
+                if result == None:
+                    print("话题[",questionId,"]不存在.")
+                else :
+                    topicId = result["id"]
+                print("插入一条新的话题[", topicId, ":", questionId, "]")
+            else:
+                topicId = result["id"]
+                print("话题[", topicId, ":", questionId, "]已存在,判断是否需要更新.")
+                # 判断questionTitle和questionSupport是否有变化,如果有变化,做更新,否则,直接跳过.
+                id = result["id"]
+                resQuestionTitle = result["title"]
+                resQuestionSupport = result["content"]
+
+                # 参数说明
+                # 1. questionTitle
+                # 2. questionSupport
+                # 3. id
+                if (questionTitle != resQuestionTitle or questionSupport != resQuestionSupport):
+                    print("话题数据变化,开始更新话题数据[", topicId, ":", questionId, "].")
+                    sql = "update pandave_topic " \
+                          "set gmt_modified=now()," \
+                          "title=%s,content=%s," \
+                          "last_follow_time=now() " \
+                          "where id=%s"
+                    cursor.execute(sql, (questionTitle, questionSupport, id))
+                    conn.commit()
+                    print("话题更新成功[", topicId, ":", questionId, "]")
+
+
+    finally:
+        conn.close()
+
+    print("话题处理完成[", topicId, ":", questionId, "]")
+
+
+# 处理话题回复信息
+def handleTopicAnswer(answerList, topicId, questionId):
+    if answerList == None:
+        print("话题[", topicId, ":", questionId, "]不存在任何回复.")
+        return
+    else:
+
+        conn = getConn()
+        try:
+            with conn.cursor() as cursor:
+
+                for answer in answerList:
+                    answerId = answer["answerId"]
+                    voteCount = answer["voteCount"]
+                    answerAuthor = answer["answerAuthor"]
+                    answerAuthorLink = answer["answerAuthorLink"]
+                    answerAuthorSign = answer["answerAuthorSign"]
+                    answerContent = answer["answerContent"]
+                    createDate = answer["createDate"]
+                    editDate = answer["editDate"]  # 点赞数量超过1000则处理
+                    print("voteCount:", voteCount)
+                    if int(voteCount) > 1000:
+                        print("回复[", answerId, "]超过1000赞,开始数据处理.")
+
+                        # 判断该回答是否已存在,如果不存在,则插入,如果已存在,则更新信息
+                        sql = "select id,topic_id,platform_aid,author_name," \
+                              "author_id,author_sign,content,answer_create_time," \
+                              "answer_modify_time,vote_count,comment_count from pandave_topic_answer where topic_id=%s and platform_aid=%s"
+                        cursor.execute(sql, (topicId, answerId))
+                        result = cursor.fetchone()
+                        if result == None:
+                            print("回复[", answerId, "]不存在,插入数据.")
+                            # 1. topicId
+                            # 2. answerId
+                            # 3. answerAuthor
+                            # 4. answerAuthorLink
+                            # 5. answerAuthorSign
+                            # 6. answerContent
+                            # 7. createDate
+                            # 8. editDate
+                            # 9. voteCount
+                            sql = "INSERT INTO pandave_topic_answer (creator, gmt_create, modifier, gmt_modified, is_deleted, topic_id, platform_aid, author_name, author_id, author_sign, content, answer_create_time, answer_modify_time, vote_count, comment_count) " \
+                                  "VALUES (1, now(), 1, now(), 'N', %s, %s, %s, %s, %s, %s, %s, %s, %s, 0);"
+                            cursor.execute(sql, (
+                                topicId, answerId, answerAuthor, answerAuthorLink, answerAuthorSign, answerContent,
+                                createDate, editDate, voteCount))
+                            conn.commit()
+                            print("回复[", answerId, "]插入完成.")
+                        else:
+                            print("更新回复[", answerId, "]数据.")
+                            id = result["id"]
+                            # 1.answerAuthor
+                            # 2.answerAuthorLink
+                            # 3.answerAuthorSign
+                            # 4.answerContent
+                            # 5.createDate
+                            # 6.editDate
+                            # 7.voteCount
+                            # 8.id
+                            sql = "update pandave_topic_answer " \
+                                  "set gmt_modified=now()," \
+                                  "author_name=%s," \
+                                  "author_id=%s," \
+                                  "author_sign=%s," \
+                                  "content=%s," \
+                                  "answer_create_time=%s," \
+                                  "answer_modify_time=%s," \
+                                  "vote_count=%s " \
+                                  "where id =%s"
+                            cursor.execute(sql, (
+                            answerAuthor, answerAuthorLink, answerAuthorSign, answerContent, createDate, editDate, voteCount,
+                            id))
+                            conn.commit()
+                    else:
+                        print("回复[", answerId, "]赞的数量不超过1000.")
+        finally:
+            conn.close()
+
+
+handleTopic(topic)
+handleTopicAnswer(answerList, topicId, questionId)
